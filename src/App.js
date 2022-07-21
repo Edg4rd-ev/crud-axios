8b13dbca4282559621a4109973a52c23325da8a5
Edgar Sá <neto.edgarsa@gmail.com>
2022-07-20 23:51:22 -0300

    Added Routers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Post from './pages/post';
+import Edit from './pages/edit';
+import Feed from './pages/feed';
+import List from './pages/list';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+  <Router>
+    <Routes>
+      <Route exact path="/" element={<Feed/>} />
+      <Route path="/post" element={<Post/>} />
+      <Route path="/edit" element={<Edit/>} />
+      <Route path="/list" element={<List/>} />
+    </Routes>
+  </Router>
   );
 }
 
