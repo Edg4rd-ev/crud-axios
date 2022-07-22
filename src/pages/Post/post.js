87761b6133d0255640cec607c91b8b6c972d0d58
Edgar Sá <neto.edgarsa@gmail.com>
2022-07-22 15:44:02 -0300

    added axios and form validations

--- src/pages/Post/post.js
+++ src/pages/Post/post.js
@@ -2,25 +2,42 @@ import React from "react";
 import Header from "../../components/header/Header"
 import {useForm} from "react-hook-form";
 import "./post.css";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import axios from "axios";
+
+const validationUser = yup.object().shape({
+  name: yup.string().required(),
+  email: yup.string().required()
+});
 
 function Post () {
-  const {register, handleSubmit, formState: {erros}} = useForm();
+  const {register, handleSubmit, formState: {errors}} = useForm({
+    resolver: yupResolver(validationUser)
+  });
+  const addUser = data => axios.post("/create_user", data)
+  .then(() => {
+    console.log("deu bom, meu bom")
+  }).catch((err) => {
+    console.log("Fudeu, my good: ", err)
+  });
   return(
     <div>
       <Header/>
       <main>
         <div className="card-post">
           <h2>Register</h2>
-          <hr className="line-post"></hr>
           <div className="card-body-post">
-            <form>
+            <form onSubmit={handleSubmit(addUser)}>
               <div className="fields">
                 <label>Name</label>
-                <input type="text" placeholder="type your name here"/>
+                <input type="text" placeholder="type your name here" name="name" {...register("name")}/>
+                <p className="error-message">{errors.name?.message}</p>
               </div>
               <div className="fields">
                 <label>E-mail</label>
-                <input type="text" placeholder="example@email.com"/>
+                <input type="text" placeholder="example@email.com" name="email" {...register("email")}/>
+                <p className="error-message">{errors.email?.message}</p>
               </div>
               <div className="btn-post">
                 <button type="submit">Add</button>
